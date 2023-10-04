48f8ec954e1262bacb93d50d880b839632f6e9aa
ngelechekiprop <ngelecheikiprop@gmail.com>
2023-10-04 22:19:36 +0000

    The foundation of start we discussed

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+/*standard libraries*/
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+/*function protypes*/
+size_t count_words(char *str, char *delim);
+char **list_of_words(char *str, char *delim);
+void exec(char **argv, char **env);
+
+#endif
