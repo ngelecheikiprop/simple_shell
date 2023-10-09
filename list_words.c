d819bfa85f1f7c32b8d4401957fdf606fee781aa
Merch7x <aquamaingi@gmail.com>
2023-10-09 17:31:12 +0300

    Add counts as a helper function to string tokenization

--- list_words.c
+++ list_words.c
@@ -1,4 +1,31 @@
 #include "shell.h"
+
+/**
+ * count_words - make a string into token
+ * @str: the string to tokenize
+ * @delim: what to use to tokenize
+ *
+ * Return: the number of words
+ *
+*/
+size_t count_words(char *str, char *delim)
+{
+	size_t count = 0;
+	char *token = NULL;
+	char *local_copy;
+
+	local_copy = strdup(str);
+	token = strtok(local_copy, delim);
+	while (token != NULL)
+	{
+		token = strtok(NULL, delim);
+		++count;
+	}
+	free(local_copy);
+	free(token);
+	return (count);
+}
+
 /**
  * list_of_words - creates a list of words from string
  * @str: the word
