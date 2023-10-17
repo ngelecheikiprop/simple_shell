bbe0ee0e1b4d3427d3347cdf2496f4c8cc279437
Merch7x <aquamaingi@gmail.com>
2023-10-17 05:29:32 +0300

    Change add node to add node to end

--- envar_list.c
+++ envar_list.c
@@ -30,37 +30,66 @@ env_list *create_list(char **env)
   list = strtok(env_copy, "\n");
   while (list)
   {
-    add_node(&head, list);
+    add_node_end(&head, list);
 		list = strtok(NULL, "\n");
   }
+  free(env_copy);
   return (head);
 }
 /**
- * add_node - Adds a new node at the beginning of a list.
+ * add_node_end - Adds a new node at the end of a list.
  * @head: A pointer to the head of the linked list.
  * @args: The string to be duplicated and added to the new node.
  *
  * Return: The address of the new element, or NULL if it failed.
  */
- 
-env_list *add_node(env_list **head, char *str)
+env_list *add_node_end(env_list **head, char *str)
 {
-	env_list *new_node;
-	new_node = malloc(sizeof(env_list));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->envar = strdup(str);
-	if (new_node->envar == NULL)
-	{
-		free(new_node);
-		return (NULL);
-	}
-	new_node->next_node = *head;
+env_list *new_node;
+env_list *last;
 
-	*head = new_node;
-	return (new_node);
+new_node = malloc(sizeof(new_node));
+if (new_node == NULL)
+{
+return (NULL);
 }
 
+new_node->envar = strdup(str);
+new_node->next_node = NULL;
 
+if (*head == NULL)
+{
+*head = new_node;
+return (new_node);
+}
+
+last = *head;
+while (last->next_node != NULL)
+{
+last = last->next_node;
+}
+last->next_node = new_node;
+return (new_node);
+}
+
+/*int print_list(env_list *head)
+{
+	env_list *current = head;
+
+	while (current != NULL)
+	{
+	if (current->envar != NULL)
+	{
+	printf("%s\n", current->envar);
+	}
+	current = current->next_node;
+	}
+	return (0);
+}
+
+int main(void)
+{
+  char **env = environ;
+  print_list(create_list(env));
+  return (0);
+}*/
