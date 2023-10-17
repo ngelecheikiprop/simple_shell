6ddb5ba4dc3e82801d4008fe1ea7f3bf8d376422
Merch7x <aquamaingi@gmail.com>
2023-10-17 11:55:09 +0300

    Add checks for null and handle path duplication

--- file_in_path.c
+++ file_in_path.c
@@ -3,6 +3,7 @@ char *file_exists(char **args)
 {
     char *env_var_name = "PATH";
     char *path = my_getenv(env_var_name);
+    char *path_copy = strdup(path);
     char *token;
     char *full_path = (char *)malloc(1024);
 
@@ -13,16 +14,16 @@ char *file_exists(char **args)
 
     if (path == NULL)
     {
-        fprintf(stderr, "PATH environment variable not found.\n");
+        perror("PATH environment variable not found");
         return (NULL);
     }
 
-    if (access(args[0], F_OK) == 0)
+    if (args[0] != NULL && access(args[0], F_OK) == 0)
     {
         return (args[0]);
     }
 
-    token = strtok(path, ":");
+    token = strtok(path_copy, ":");
     while (token != NULL)
     {
         snprintf(full_path, 1024, "%s/%s", token, args[0]);
@@ -39,7 +40,7 @@ char *file_exists(char **args)
 
 /*int main(void)
 {
-  char *args[10] = {"ls"};
+  char *args[10] = {"/bin/ls"};
   char *file_stat = file_exists(args);
   if (file_stat != NULL) {
       printf("%s", file_stat);
