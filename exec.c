a3cb5bc8ed17dc0759799d62e245b5dbce4a3a70
Merch7x <aquamaingi@gmail.com>
2023-10-18 04:28:29 +0300

    Test

---
+++ exec.c
@@ -0,0 +1,23 @@
+#include "my_shell.h"
+/**
+ * exec - execute a command
+ * @argv: the array of commands
+ * @env: the environment
+ *
+ * Return: nothing
+*/
+void exec(char **argv, char **env)
+{
+        pid_t child;
+        int status;
+        child = fork();
+        if (child == 0)
+        {
+                execve(argv[0], argv, env);
+        }
+        else
+        {
+                waitpid(child, &status, 0);
+                free(argv);
+        }
+}
---
+++ my_shell.c
@@ -0,0 +1,40 @@
+#include "my_shell.h"
+/**
+ * main -this is the entry point
+ * @ac: this if the argument count
+ * @av: this contain the arguments
+ * @env: the environment variable
+ *
+ * Return: nothing
+*/
+int main(int ac, char **av, char **env)
+{
+size_t count;
+char *buffer = NULL;
+char **argv; 
+char *delim = " ";
+char *prompt = "$";
+
+(void)ac;
+(void)av;
+while (1)
+{
+buffer = NULL;
+count = 0;
+if (isatty(fileno(stdin)))
+{
+write(STDOUT_FILENO, prompt, 1);
+fflush(stdout);
+}
+if (getline(&buffer, &count, stdin) == EOF)
+{
+        free(buffer);
+        exit(100);
+}
+strtok(buffer, "\n");
+argv = list_of_words(buffer, delim);
+exec(argv, env);
+free(buffer);
+}
+return (0);
+}
